d8f680852c6c7e0bd556e7944b3b15bbb035d9e0
Melvyn Malherbe <melvyn.malherbe@qoqa.com>
2020-01-19 01:30:52 +0100

    Add test in React

---
+++ precache-manifest.4fefbfa1d9bf4f00996c0024542ce541.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d6ad4d60e3dbad3e12fa6fb9de84d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "788f07438159c3cbe38a",
+    "url": "/static/css/main.d514b748.chunk.css"
+  },
+  {
+    "revision": "34edcdc35144cfdbcfb6",
+    "url": "/static/js/2.ada87c0a.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.ada87c0a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "788f07438159c3cbe38a",
+    "url": "/static/js/main.5541d994.chunk.js"
+  },
+  {
+    "revision": "da6d9570104af6bff2fb",
+    "url": "/static/js/runtime-main.6bda29f8.js"
+  }
+]);
