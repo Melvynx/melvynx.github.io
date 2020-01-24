5a32e3f8530ce2de9ee8171e4401ff4f540162ac
Melvyn Malherbe <melvyn.malherbe@qoqa.com>
2020-01-24 10:18:49 +0100

    Add button for stop redirect

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+let timerEnable = true;
 function onLoad() {
   startTimer();
   setTimeout(() => {
-    window.location.replace("https://melvynx.github.io/tools/#/");
+    if (timerEnable) {
+      window.location.replace("https://melvynx.github.io/tools/#/");
+    }
   }, 6000);
 }
 
@@ -12,11 +15,25 @@ function startTimer() {
   if (timerTime < 0) {
     return;
   }
+  if (!timerEnable) {
+    return;
+  }
   setTimeout(() => {
     timer.innerHTML = timerTime;
     timerTime--;
+
     startTimer();
   }, 900);
+}
+
+function stopTimer() {
+  timer.style.backgroundColor = "#e91e63";
+  timer.style.color = "white";
+  const stop = document.getElementById("buttonStop");
+  stop.classList.remove("btn-danger");
+  stop.classList.add("btn-success");
+  stop.innerHTML = "It's stopped";
+  timerEnable = false;
 }
 
 onLoad();
